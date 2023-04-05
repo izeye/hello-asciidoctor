0b1fc887e99d6e5c2d5333a3e6e378f428c7772d
Johnny Lim <izeye@naver.com>
2023-04-05 19:42:00 +0900

    Add hello world for Asciidoctor

---
+++ src/main/java/com/izeye/helloworld/asciidoctor/HelloAsciidoctorApplication.java
@@ -0,0 +1,13 @@
+package com.izeye.helloworld.asciidoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAsciidoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAsciidoctorApplication.class, args);
+	}
+
+}
